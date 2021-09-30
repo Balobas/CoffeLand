5bf1094fc61f5d6386647115b8d833bc98245be3
Балобанов Владислав <vbalobanov@innoseti.ru>
2021-09-30 15:57:58 +0200

    feat(core): add blog post use cases

---
+++ app/core/usecases/blogPost.go
@@ -0,0 +1,38 @@
+package usecases
+
+import (
+	"CoffeLand/app/core/data"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
+type BlogPostUsecases struct {
+	data.Core
+}
+
+func NewBlogPostUsecases(core data.Core) *BlogPostUsecases {
+	return &BlogPostUsecases{core}
+}
+
+func(b *BlogPostUsecases) GetByTitleLike(name string) ([]data.BlogPost, error) {
+	return b.BlogPostRepo.GetByTitleLike(name)
+}
+
+func(b *BlogPostUsecases) GetAll() ([]data.BlogPost, error) {
+	return b.BlogPostRepo.GetAll()
+}
+
+func(b *BlogPostUsecases) put(post data.BlogPost) (string, error) {
+	if err := post.Validate(); err != nil {
+		return "", errors.WithStack(err)
+	}
+	if len(post.ID) == 0 {
+		post.ID = uuid.New().String()
+	}
+
+	if err := b.BlogPostRepo.Store(post); err != nil {
+		return "",  errors.WithStack(err)
+	}
+
+	return post.ID, nil
+}
