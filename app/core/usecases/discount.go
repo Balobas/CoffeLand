5469bd7f522eb890b2e500be13d6f00b7d452dd2
Балобанов Владислав <vbalobanov@innoseti.ru>
2021-09-30 15:54:38 +0200

    feat(core): add discount use cases

---
+++ app/core/usecases/discount.go
@@ -0,0 +1,40 @@
+package usecases
+
+import (
+	"CoffeLand/app/core/data"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
+type DiscountUsecases struct {
+	data.Core
+}
+
+func NewDiscountUsecases(core data.Core) *DiscountUsecases {
+	return &DiscountUsecases{core}
+}
+
+func(d *DiscountUsecases) GetByNameLike(name string) ([]data.Discount, error) {
+	return d.Core.DiscountRepo.GetByNameLike(name)
+}
+
+func(d *DiscountUsecases) GetActual() ([]data.Discount, error) {
+	return d.Core.DiscountRepo.GetActual()
+}
+
+func(d *DiscountUsecases) GetByProductType(productType data.ProductType) ([]data.Discount, error) {
+	return d.Core.DiscountRepo.GetByProductType(productType)
+}
+
+func(d *DiscountUsecases) putDiscount(discount data.Discount) (string, error) {
+	if err := discount.Validate(); err != nil {
+		return "", errors.WithStack(err)
+	}
+	if len(discount.ID) == 0 {
+		discount.ID = uuid.New().String()
+	}
+	if err := d.Core.DiscountRepo.Store(discount); err != nil {
+		return discount.ID, errors.WithStack(err)
+	}
+	return discount.ID, nil
+}
