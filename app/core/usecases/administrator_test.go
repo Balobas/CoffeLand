e9709ab1c0d7f67cd1a9c55aab6a3f11b8410716
Балобанов Владислав <vbalobanov@innoseti.ru>
2021-09-30 15:58:59 +0200

    feat(core): add administrator use cases tests

---
+++ app/core/usecases/administrator_test.go
@@ -0,0 +1,80 @@
+package usecases
+
+import (
+	"CoffeLand/app/core/data"
+	"fmt"
+	"testing"
+)
+
+type AdminRepoMock struct {
+
+}
+
+func (a AdminRepoMock) Store(data.Administrator) error {
+	return nil
+}
+
+func (a AdminRepoMock) GetByID(string) (data.Administrator, error) {
+	return data.Administrator{
+		ID:         "123",
+		FirstName:  "",
+		LastName:   "",
+		Patronymic: "",
+		Password:   "",
+		Access:     data.AdministratorAccessMiddle,
+		Hash:       nil,
+	}, nil
+}
+
+func (a AdminRepoMock) GetByFIO(firstName string, lastName string, patronymic string) ([]data.Administrator, error) {
+	return []data.Administrator{}, nil
+}
+
+var (
+	CoreMock = data.Core{
+		AdminRepo:    AdminRepoMock{},
+		DiscountRepo: nil,
+		ProductRepo:  nil,
+		BlogPostRepo: nil,
+		Hasher:       nil,
+		Auth:         nil,
+	}
+)
+
+
+func TestNewAdministratorUsecases(t *testing.T) {
+	adminUsecases, err := NewAdministratorUsecases(CoreMock, "123")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	fmt.Println(adminUsecases.Admin)
+}
+
+func TestAdministratorUsecases_PutAdministrator(t *testing.T) {
+	adminUsecases, err := NewAdministratorUsecases(CoreMock, "123")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	_, err = adminUsecases.PutAdministrator(data.Administrator{
+		ID:         "",
+		FirstName:  "vasya",
+		LastName:   "petrov",
+		Patronymic: "petrovich",
+		Password:   "",
+		Access:     data.AdministratorAccessLow,
+		Hash:       nil,
+	})
+
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Errorf("expected error")
+		t.FailNow()
+	}
+}
+
+
