021a24054d2e251026ffb7c1ddb9d1b5030585e3
Балобанов Владислав <vbalobanov@innoseti.ru>
2021-09-30 15:53:29 +0200

    feat(core): add Core struct

---
+++ app/core/data/data.go
@@ -0,0 +1,24 @@
+package data
+
+type ProductType string
+
+const (
+	ProductTypeCoffee = ProductType("coffee")
+)
+
+func(pt ProductType) IsValid() bool {
+	switch pt {
+	case ProductTypeCoffee:
+		return true
+	default:
+		return false
+	}
+}
+
+type Core struct {
+	AdminRepo AdministratorRepository
+	DiscountRepo DiscountsRepository
+	ProductRepo ProductsRepository
+	BlogPostRepo BlogPostRepository
+}
+
